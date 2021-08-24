819d175bd378737052ecfb56365c42c9a5776fbc
Pedro Soares <pedromtec@gmail.com>
2021-08-24 20:33:36 -0300

    Add env variable and create first query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
 import './App.css';
 
+import { gql, useQuery } from '@apollo/client'
+
+const organizationId = '300562393'
+
+const ORGANIZATION_QUERY = gql`
+  query Organization($organizationId: ID!) {
+    organization(id: $organizationId) {
+      pipes {
+        color
+        cards_count
+        name
+        id
+        icon 
+      }
+    }
+}
+`
+
 function App() {
+
+  const { data, loading } = useQuery(ORGANIZATION_QUERY, {
+    variables: {
+      organizationId
+    }
+  })
+
+  if(loading) {
+    return <p>Loading ...</p>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +40,12 @@ function App() {
           alt="logo"
         />
       </header>
+
+      {data.organization.pipes.map(pipe => (
+        <div key={pipe.id}>
+          <h2>{pipe.name}</h2>
+        </div>
+      ))}
     </div>
   );
 }
